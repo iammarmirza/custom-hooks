b7799e298072f16e946e27a2c1516c6f99003fcb
Ammar Mirza <itsammarmirza@gmail.com>
2024-04-26 15:41:47 +0530

    useThrottle Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { NavLink } from 'react-router-dom'
 import './App.css'
-import { useFetch } from './hooks/useFetch'
+import { useState } from 'react'
+import { useDebounce } from './hooks/useDebounce'
+import { useThrottle } from './hooks/useThrottle'
 
 function App() {
-  const myFetch = useFetch('https://api.jikan.moe/v4/anime', {
-    q: 'Naruto',
-    page: 1
-  },
-    {
-      shouldCancelOnUnmount: true
-    }
-  )
-  const { data, error, loading, fetchData } = myFetch
-  console.log('data', data)
-  console.log('error', error)
-  console.log('loading', loading)
+  const [text, setText] = useState('')
+  const debounced = useDebounce(text, 1000)
+  const throttled = useThrottle(text, 1000)
+ 
   return (
     <>
-      <button onClick={fetchData}>Click me</button>
-      <NavLink to={'/secondary'} >Secondary page</NavLink>
+      <input type='text' placeholder='Enter text' value={text} onChange={(e) => setText(e.currentTarget.value)} />
+      <div>
+        <span>Default Text : {text}</span>
+      </div>
+      <div>
+        <span>Debounced Text : {debounced}</span>
+      </div>
+      <div>
+        <span>Throttled Text : {throttled}</span>
+      </div>
     </>
   )
 }
